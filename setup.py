6a203ae25dce45f04b2a7fc47e49439a0601faac
Dagwbl <39078825+Dagwbl@users.noreply.github.com>
2021-12-27 19:11:58 +0800

    The first version

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='pygwbl', 
+    version='1.1.1',
+    py_modules =['pygwbl'], #这个要跟发布的模块名一致
+    author='Dagwbl',
+    author_email='Dagwbl@qq.com',
+    url='',
+    description='这是我的第一个发布'
+)
